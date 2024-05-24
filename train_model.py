dfb12ebe5a9d7ec5735319263a8095aad74ee81c
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2024-05-24 18:00:19 +0200

    predict_price: to be fixed

--- train_model.py
+++ train_model.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 from tools import setTheta, createAnimation
 import os
 from LinearRegression import LinearRegression
+import multiprocessing
+
+
+def save_animation(ani):
+    print("Writing to animation.gif...")
+    ani.save("animation.gif")
+    print("Done!")
 
 
 def main():
@@ -12,7 +19,7 @@ def main():
         print("File not found")
         return
     except Exception as e:
-        print(f"An error occured: {e}")
+        print(f"An error occurred: {e}")
         return
     x = data["km"].to_numpy().reshape(-1, 1)
     y = data["price"].to_numpy().reshape(-1, 1)
@@ -22,14 +29,16 @@ def main():
     model = LinearRegression()
     model.fit(x, y)
     setTheta(model.t0, model.t1)
-
     ani = createAnimation(x, model)
+
     plt.title("Price of a car for a given mileage (prediction)")
     plt.scatter(x, y, c="b")
     plt.xlabel("Mileage")
     plt.ylabel("Price")
-    ani.save("animation.gif")
+    save_process = multiprocessing.Process(target=save_animation, args=(ani,))
+    save_process.start()
     plt.show()
+    save_process.join()
 
     plt.title("Algorithm precision")
     plt.plot(model.loss, c="r")
